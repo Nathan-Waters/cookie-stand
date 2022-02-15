eb166ea5780b571683a38a4b63e24e150082181c
Nathan Waters <nathanwaters94@gmail.com>
2022-02-15 15:44:58 -0800

    complete seattle

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 //the hours that the store will be open
 let openHours = ['6am:', '7am:','8am:', '9am:', '10am:', '11am:', '12pm:','1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm', '7pm:'];
 
-//start of city objects
+//stop 1: window into the DOM
+// 1 way - document.getElementById - method that will take a string for the ID
+// 2nd - document.querySelector - method that takes in a string, id, class, element type
 
+let htmlBodyWindow = document.getElementById('sales');
+console.log(htmlBodyWindow);
+console.dir(htmlBodyWindow);
+
+
+//start of city objects
 //seattle object
 let seattle = {
   name: 'Seattle',
@@ -74,16 +82,15 @@ let lima = {
     this.randomC = Math.round((random(this.minV, this.maxV)) * this.avgC);
   }
 };
-// funtion that takes two numbers and picks a random number between them.
 
+// funtion that takes two numbers and picks a random number between them.
 function random(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
-seattle.customers();
-console.log(seattle);
-// seattle.customers();
+
+//function that creates teh array for each store
 function createArr(arr){
   for(let i = 0; i < openHours.length; i++){
     arr.customers();
@@ -91,6 +98,34 @@ function createArr(arr){
     arr.totalC += arr.randomC;
   }
 }
+
+// DOM Manipulation
+
+//step 1 - js needs a window into the dom normally at the top 
+
+seattle.render = function(){
+
+  createArr(this);
+  //step 2 - js will create an element - document.createElement - method, string of the html element you want to create
+  let h2Elem = document.createElement('h2');
+  //step 3 - give it context if needed
+  h2Elem.textContent = this.name;
+  //step 4 - add it to the DOM -- parent.appendChild(child)
+  htmlBodyWindow.appendChild(h2Elem);
+
+  let ulElem = document.createElement('ul');
+  htmlBodyWindow.appendChild(ulElem);
+
+  for(let i = 0; i < this.custEachHour.length; i++){
+    let hour = this.custEachHour[i];
+    let liElem = document.createElement('li');
+    liElem.textContent = hour;
+    ulElem.appendChild(liElem);
+  }
+
+};
+
+seattle.render();
 
 createArr(seattle);
 console.log(seattle.custEachHour);
