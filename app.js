f4a065196eab4ae5f31cf5df9bc1d3e8d2a92464
Nathan Waters <nathanwaters94@gmail.com>
2022-02-16 16:23:48 -0800

    adding table to html, finishing lab

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let htmlBodyWindow = document.getElementById('sales');
-let table = document.querySelector('table');
-let openHours = ['6am:', '7am:','8am:', '9am:', '10am:', '11am:', '12pm:','1pm:', '2pm:', '3pm:', '4pm:', '5pm:', '6pm', '7pm:'];
+let htmlBodyWindow = document.getElementById('table');
+// let table = document.querySelector('table');
+let openHours = ['6am', '7am','8am', '9am', '10am', '11am', '12pm','1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 let storeCreation = [];
 
 //constructor - starts with function and capital letters
@@ -10,7 +10,7 @@ let storeCreation = [];
 
 function Store(name, minV, maxV, avgC){
   this.storeName = name;
-  this.minimumValue =  minV;
+  this.minimumValue = minV;
   this.maximumValue = maxV;
   this.averageCustomer = avgC;
   this.randomC = 0;
@@ -25,7 +25,7 @@ function random(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
-//function that creates teh array for each store
+// function that creates teh array for each store
 Store.prototype.createArr = function (arr){
   for(let i = 0; i < openHours.length; i++){
     arr.randomize();
@@ -34,53 +34,66 @@ Store.prototype.createArr = function (arr){
   }
 };
 
-//
+// used in the function above to make the values in the array
 Store.prototype.randomize = function() {
   this.randomC = Math.round((random(this.minimumValue, this.maximumValue)) * this.averageCustomer);
 };
-
-//creating the table
-Store.prototype.render = function(){
-  let h2Elem = document.createElement('h2');
-  h2Elem.textContent = this.storeName;
-  htmlBodyWindow.appendChild(h2Elem);
-
-  //table render to DOM
-
-  // let row1 = document.createElement('tr');
-  // table.appendChild(row1);
-
-  // let th1Elem = document.createElement('th');
-  // th1Elem.textContent = openHours[0];
-  // row1.appendChild(th1Elem);
-  // let th2Elem = document.createElement('th');
-  // th2Elem.textContent = storeCreation;
-  // row1.appendChild(th2Elem);
-  // let th3Elem = document.createElement('th');
-  // th3Elem.textContent = this.averageCustomer;
-  // row1.appendChild(th3Elem);
+// renderst the actual body of content
+//displays the names of each row
+Store.prototype.renderBody = function(){
+  for(let i = 0; i < storeCreation.length; i++){
+    let currentCity = storeCreation[i];
+    let createR = document.createElement('tr');
+    let createD = document.createElement('td');
+    createD.textContent = currentCity.storeName;
+    htmlBodyWindow.appendChild(createR);
+    createR.appendChild(createD);
+    //shows pulls the data out of the arrays 
+    for(let j = 0; j < currentCity.custEachHour.length; j++){
+      let hourlyCust = currentCity.custEachHour[j];
+      let createNewD = document.createElement('td');
+      createNewD.textContent = hourlyCust;
+      createR.appendChild(createNewD);
+    }
+    //adds up each array before looping to the next one
+    let totalCookies = document.createElement('td');
+    totalCookies.textContent = currentCity.totalC;
+    createR.appendChild(totalCookies);
+  }
 };
+//creats the top or header bar of the table
+function renderHeader(){
+  let thElem = document.createElement('tr');
+  let trElem = document.createElement('th');
+  htmlBodyWindow.appendChild(thElem);
+  thElem.appendChild(trElem);
+  trElem.textContent = 'City';
+  for(let i = 0; i < openHours.length; i++){
+    let time = openHours[i];
+    let header = document.createElement('th');
+    header.textContent = time;
+    thElem.appendChild(header);
+  }
+  let total = document.createElement('th');
+  total.textContent = 'Daily Location Total';
+  thElem.appendChild(total);
+}
 
+//initalizes all of the objects
 let seattle = new Store('Seattle', 23, 65, 6.3);
 let tokyo = new Store('Tokyo', 3, 24, 1.2);
 let dubai = new Store('Dubai', 11, 38, 3.7);
 let paris = new Store('Paris', 20, 38, 2.3);
 let lima = new Store('Lima', 2, 16, 4.6);
-
-console.log(seattle);
-console.log(storeCreation);
-
-function renderAllStores() {
+//creates the arrays in our objects
+function renderAllStoresO() {
   for (let i = 0; i < storeCreation.length; i++) {
     let currentStore = storeCreation[i];
-    // currentStore.randomize();
     currentStore.createArr(storeCreation[i]);
-    currentStore.render();
   }
 }
+renderAllStoresO();
 
-renderAllStores();
-
-console.log(seattle);
-console.log(paris);
-console.log(storeCreation);
+//creation!
+renderHeader();
+seattle.renderBody();
